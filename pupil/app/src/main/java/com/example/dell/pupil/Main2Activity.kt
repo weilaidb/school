e3e6d15cafb1115c3b763d3dae58fa6763d5b230
yjduoduo <wxjlmr@126.com>
2019-09-03 07:24:10 +0800

    使用原生代码生成 笔顺

---
+++ pupil/app/src/main/java/com/example/dell/pupil/Main2Activity.kt
@@ -0,0 +1,19 @@
+package com.example.dell.pupil
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View  //记得导入所需文件
+import android.widget.Toast //记得导入所需文件
+
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main2)
+//        textview.text = "sdfadsfds" //直接使用布局中定义的id，不需要获取
+    }
+}
--- pupil/app/src/main/java/com/example/dell/pupil/MainActivity.kt
+++ pupil/app/src/main/java/com/example/dell/pupil/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.dell.pupil
-
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View  //记得导入所需文件
+import android.widget.Toast //记得导入所需文件
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,20 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+        button2.setOnClickListener(this)
+
+    }
+
+    fun onClick(v: View?) {
+//        textView.setText("超级酷")           //这是一个点击按钮后执行的代码，点击按钮这个操作之后，textview的文字被设置为超级酷
+        showToast("测试成功")
+
+    }
+
+    fun  showToast(str:String) //       写一个方法显示测试成功，浮现于程序表面，数秒之后消失
+    {
+        Toast.makeText(this,str,Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -27,4 +46,14 @@ class MainActivity : AppCompatActivity() {
             System.loadLibrary("native-lib")
         }
     }
+
+    private fun Button.setOnClickListener(mainActivity: MainActivity) {
+        showToast("测试成功 +OK")
+//        val intent = Intent()
+//        //获取intent对象
+//        intent.setClass(this,Main2Activity::class.java)
+//        // 获取class是使用::反射
+//        startActivity(intent)
+    }
 }
+
